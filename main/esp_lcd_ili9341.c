ac0c46ea6fe95ae0e86578497fb3ab445b14457d
szwaymi <61036584+szwaymi@users.noreply.github.com>
2022-08-31 16:19:23 +0800

    This is a very simple test application for ESP32 with ILI9341 LCD Driver IC.

---
+++ main/esp_lcd_ili9341.c
@@ -0,0 +1,305 @@
+/*
+ * SPDX-FileCopyrightText: 2022 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <stdlib.h>
+#include <sys/cdefs.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_lcd_panel_interface.h"
+#include "esp_lcd_panel_io.h"
+#include "esp_lcd_panel_vendor.h"
+#include "esp_lcd_panel_ops.h"
+#include "esp_lcd_panel_commands.h"
+#include "driver/gpio.h"
+#include "esp_log.h"
+#include "esp_check.h"
+
+static const char *TAG = "ili9341";
+
+static esp_err_t panel_ili9341_del(esp_lcd_panel_t *panel);
+static esp_err_t panel_ili9341_reset(esp_lcd_panel_t *panel);
+static esp_err_t panel_ili9341_init(esp_lcd_panel_t *panel);
+static esp_err_t panel_ili9341_draw_bitmap(esp_lcd_panel_t *panel, int x_start, int y_start, int x_end, int y_end, const void *color_data);
+static esp_err_t panel_ili9341_invert_color(esp_lcd_panel_t *panel, bool invert_color_data);
+static esp_err_t panel_ili9341_mirror(esp_lcd_panel_t *panel, bool mirror_x, bool mirror_y);
+static esp_err_t panel_ili9341_swap_xy(esp_lcd_panel_t *panel, bool swap_axes);
+static esp_err_t panel_ili9341_set_gap(esp_lcd_panel_t *panel, int x_gap, int y_gap);
+static esp_err_t panel_ili9341_disp_on_off(esp_lcd_panel_t *panel, bool off);
+
+typedef struct {
+    esp_lcd_panel_t base;
+    esp_lcd_panel_io_handle_t io;
+    int reset_gpio_num;
+    bool reset_level;
+    int x_gap;
+    int y_gap;
+    unsigned int bits_per_pixel;
+    uint8_t madctl_val; // save current value of LCD_CMD_MADCTL register
+    uint8_t colmod_cal; // save surrent value of LCD_CMD_COLMOD register
+} ili9341_panel_t;
+
+esp_err_t esp_lcd_new_panel_ili9341(const esp_lcd_panel_io_handle_t io, const esp_lcd_panel_dev_config_t *panel_dev_config, esp_lcd_panel_handle_t *ret_panel)
+{
+    esp_err_t ret = ESP_OK;
+    ili9341_panel_t *ili9341 = NULL;
+    ESP_GOTO_ON_FALSE(io && panel_dev_config && ret_panel, ESP_ERR_INVALID_ARG, err, TAG, "invalid argument");
+    ili9341 = calloc(1, sizeof(ili9341_panel_t));
+    ESP_GOTO_ON_FALSE(ili9341, ESP_ERR_NO_MEM, err, TAG, "no mem for ili9341 panel");
+
+    if (panel_dev_config->reset_gpio_num >= 0) {
+        gpio_config_t io_conf = {
+            .mode = GPIO_MODE_OUTPUT,
+            .pin_bit_mask = 1ULL << panel_dev_config->reset_gpio_num,
+        };
+        ESP_GOTO_ON_ERROR(gpio_config(&io_conf), err, TAG, "configure GPIO for RST line failed");
+    }
+
+    switch (panel_dev_config->color_space) {
+    case ESP_LCD_COLOR_SPACE_RGB:
+        ili9341->madctl_val = 0;
+        break;
+    case ESP_LCD_COLOR_SPACE_BGR:
+        ili9341->madctl_val |= LCD_CMD_BGR_BIT;
+        break;
+    default:
+        ESP_GOTO_ON_FALSE(false, ESP_ERR_NOT_SUPPORTED, err, TAG, "unsupported color space");
+        break;
+    }
+
+    switch (panel_dev_config->bits_per_pixel) {
+    case 16:
+        ili9341->colmod_cal = 0x55;
+        break;
+    case 18:
+        ili9341->colmod_cal = 0x66;
+        break;
+    default:
+        ESP_GOTO_ON_FALSE(false, ESP_ERR_NOT_SUPPORTED, err, TAG, "unsupported pixel width");
+        break;
+    }
+
+    ili9341->io = io;
+    ili9341->bits_per_pixel = panel_dev_config->bits_per_pixel;
+    ili9341->reset_gpio_num = panel_dev_config->reset_gpio_num;
+    ili9341->reset_level = panel_dev_config->flags.reset_active_high;
+    ili9341->base.del = panel_ili9341_del;
+    ili9341->base.reset = panel_ili9341_reset;
+    ili9341->base.init = panel_ili9341_init;
+    ili9341->base.draw_bitmap = panel_ili9341_draw_bitmap;
+    ili9341->base.invert_color = panel_ili9341_invert_color;
+    ili9341->base.set_gap = panel_ili9341_set_gap;
+    ili9341->base.mirror = panel_ili9341_mirror;
+    ili9341->base.swap_xy = panel_ili9341_swap_xy;
+#if ESP_IDF_VERSION < ESP_IDF_VERSION_VAL(5, 0, 0)
+    ili9341->base.disp_off = panel_ili9341_disp_on_off;
+#else
+    ili9341->base.disp_on_off = panel_ili9341_disp_on_off;
+#endif
+    *ret_panel = &(ili9341->base);
+    ESP_LOGD(TAG, "new ili9341 panel @%p", ili9341);
+    return ESP_OK;
+
+err:
+    if (ili9341) {
+        if (panel_dev_config->reset_gpio_num >= 0) {
+            gpio_reset_pin(panel_dev_config->reset_gpio_num);
+        }
+        free(ili9341);
+    }
+    return ret;
+}
+
+static esp_err_t panel_ili9341_del(esp_lcd_panel_t *panel)
+{
+    ili9341_panel_t *ili9341 = __containerof(panel, ili9341_panel_t, base);
+
+    if (ili9341->reset_gpio_num >= 0) {
+        gpio_reset_pin(ili9341->reset_gpio_num);
+    }
+    ESP_LOGD(TAG, "del ili9341 panel @%p", ili9341);
+    free(ili9341);
+    return ESP_OK;
+}
+
+static esp_err_t panel_ili9341_reset(esp_lcd_panel_t *panel)
+{
+    ili9341_panel_t *ili9341 = __containerof(panel, ili9341_panel_t, base);
+    esp_lcd_panel_io_handle_t io = ili9341->io;
+
+    // perform hardware reset
+    if (ili9341->reset_gpio_num >= 0) {
+        gpio_set_level(ili9341->reset_gpio_num, ili9341->reset_level);
+        vTaskDelay(pdMS_TO_TICKS(10));
+        gpio_set_level(ili9341->reset_gpio_num, !ili9341->reset_level);
+        vTaskDelay(pdMS_TO_TICKS(10));
+    } else { // perform software reset
+        esp_lcd_panel_io_tx_param(io, LCD_CMD_SWRESET, NULL, 0);
+        vTaskDelay(pdMS_TO_TICKS(20)); // spec, wait at least 5ms before sending new command
+    }
+
+    return ESP_OK;
+}
+
+typedef struct {
+    uint8_t cmd;
+    uint8_t data[16];
+    uint8_t data_bytes; // Length of data in above data array; 0xFF = end of cmds.
+} lcd_init_cmd_t;
+static const lcd_init_cmd_t vendor_specific_init[] = {
+    /* SW reset */
+    {LCD_CMD_SWRESET, {0}, 0},
+    /* Sleep out */
+    {LCD_CMD_SLPOUT, {0}, 0x80},
+    {0xB6, {0x0A, 0xA2}, 2},
+    /* Display on */
+    {LCD_CMD_DISPON, {0}, 0x80},
+    /* Invert colors */
+    //{LCD_CMD_INVOFF, {0}, 0},
+
+    {0, {0}, 0xff},
+};
+
+static esp_err_t panel_ili9341_init(esp_lcd_panel_t *panel)
+{
+    printf("ILI 9341 Init\n");
+    ili9341_panel_t *ili9341 = __containerof(panel, ili9341_panel_t, base);
+    esp_lcd_panel_io_handle_t io = ili9341->io;
+
+    // LCD goes into sleep mode and display will be turned off after power on reset, exit sleep mode first
+    esp_lcd_panel_io_tx_param(io, LCD_CMD_SLPOUT, NULL, 0);
+    vTaskDelay(pdMS_TO_TICKS(100));
+    esp_lcd_panel_io_tx_param(io, LCD_CMD_MADCTL, (uint8_t[]) {
+        //ili9341->madctl_val,
+        0x28,
+    }, 1);
+    printf("%X\n",ili9341->madctl_val);
+    esp_lcd_panel_io_tx_param(io, LCD_CMD_COLMOD, (uint8_t[]) {
+        ili9341->colmod_cal,
+    }, 1);
+    printf("%X\n",ili9341->colmod_cal);
+    // vendor specific initialization, it can be different between manufacturers
+    // should consult the LCD supplier for initialization sequence code
+    int cmd = 0;
+    while (vendor_specific_init[cmd].data_bytes != 0xff) {
+        printf("%d:%X (%d)\n",cmd ,vendor_specific_init[cmd].cmd, vendor_specific_init[cmd].data_bytes & 0x1F);
+        esp_lcd_panel_io_tx_param(io, vendor_specific_init[cmd].cmd, vendor_specific_init[cmd].data, vendor_specific_init[cmd].data_bytes & 0x1F);
+        cmd++;
+    }
+
+    esp_lcd_panel_io_tx_param(io, vendor_specific_init[cmd].cmd, vendor_specific_init[cmd].data, vendor_specific_init[cmd].data_bytes & 0x1F);
+    return ESP_OK;
+}
+
+static esp_err_t panel_ili9341_draw_bitmap(esp_lcd_panel_t *panel, int x_start, int y_start, int x_end, int y_end, const void *color_data)
+{
+    
+    ili9341_panel_t *ili9341 = __containerof(panel, ili9341_panel_t, base);
+    assert((x_start < x_end) && (y_start < y_end) && "start position must be smaller than end position");
+    esp_lcd_panel_io_handle_t io = ili9341->io;
+
+    //x_start += ili9341->x_gap;
+    //x_end += ili9341->x_gap;
+    //y_start += ili9341->y_gap;
+    //y_end += ili9341->y_gap;
+
+    
+    // define an area of frame memory where MCU can access
+    esp_lcd_panel_io_tx_param(io, LCD_CMD_CASET, (uint8_t[]) {
+        (x_start >> 8) & 0xFF,
+        x_start & 0xFF,
+        ((x_end - 1) >> 8) & 0xFF,
+        (x_end - 1) & 0xFF,
+    }, 4);
+    esp_lcd_panel_io_tx_param(io, LCD_CMD_RASET, (uint8_t[]) {
+        (y_start >> 8) & 0xFF,
+        y_start & 0xFF,
+        ((y_end - 1) >> 8) & 0xFF,
+        (y_end - 1) & 0xFF,
+    }, 4);
+    // transfer frame buffer
+    size_t len = (x_end - x_start) * (y_end - y_start) * ili9341->bits_per_pixel / 8;
+    //printf("Len = %d %d\n", len);
+    esp_lcd_panel_io_tx_color(io, LCD_CMD_RAMWR, color_data, len);
+    
+   printf("Draw\n");
+    return ESP_OK;
+}
+
+static esp_err_t panel_ili9341_invert_color(esp_lcd_panel_t *panel, bool invert_color_data)
+{
+    ili9341_panel_t *ili9341 = __containerof(panel, ili9341_panel_t, base);
+    esp_lcd_panel_io_handle_t io = ili9341->io;
+    int command = 0;
+    if (invert_color_data) {
+        command = LCD_CMD_INVON;
+    } else {
+        command = LCD_CMD_INVOFF;
+    }
+    esp_lcd_panel_io_tx_param(io, command, NULL, 0);
+    return ESP_OK;
+}
+
+static esp_err_t panel_ili9341_mirror(esp_lcd_panel_t *panel, bool mirror_x, bool mirror_y)
+{
+    ili9341_panel_t *ili9341 = __containerof(panel, ili9341_panel_t, base);
+    esp_lcd_panel_io_handle_t io = ili9341->io;
+    if (mirror_x) {
+        ili9341->madctl_val |= LCD_CMD_MX_BIT;
+    } else {
+        ili9341->madctl_val &= ~LCD_CMD_MX_BIT;
+    }
+    if (mirror_y) {
+        ili9341->madctl_val |= LCD_CMD_MY_BIT;
+    } else {
+        ili9341->madctl_val &= ~LCD_CMD_MY_BIT;
+    }
+    esp_lcd_panel_io_tx_param(io, LCD_CMD_MADCTL, (uint8_t[]) {
+        ili9341->madctl_val
+    }, 1);
+    return ESP_OK;
+}
+
+static esp_err_t panel_ili9341_swap_xy(esp_lcd_panel_t *panel, bool swap_axes)
+{
+    ili9341_panel_t *ili9341 = __containerof(panel, ili9341_panel_t, base);
+    esp_lcd_panel_io_handle_t io = ili9341->io;
+    if (swap_axes) {
+        ili9341->madctl_val |= LCD_CMD_MV_BIT;
+    } else {
+        ili9341->madctl_val &= ~LCD_CMD_MV_BIT;
+    }
+    esp_lcd_panel_io_tx_param(io, LCD_CMD_MADCTL, (uint8_t[]) {
+        ili9341->madctl_val
+    }, 1);
+    return ESP_OK;
+}
+
+static esp_err_t panel_ili9341_set_gap(esp_lcd_panel_t *panel, int x_gap, int y_gap)
+{
+    ili9341_panel_t *ili9341 = __containerof(panel, ili9341_panel_t, base);
+    ili9341->x_gap = x_gap;
+    ili9341->y_gap = y_gap;
+    return ESP_OK;
+}
+
+static esp_err_t panel_ili9341_disp_on_off(esp_lcd_panel_t *panel, bool on_off)
+{
+    ili9341_panel_t *ili9341 = __containerof(panel, ili9341_panel_t, base);
+    esp_lcd_panel_io_handle_t io = ili9341->io;
+    int command = 0;
+
+#if ESP_IDF_VERSION < ESP_IDF_VERSION_VAL(5, 0, 0)
+    on_off = !on_off;
+#endif
+
+    if (on_off) {
+        command = LCD_CMD_DISPON;
+    } else {
+        command = LCD_CMD_DISPOFF;
+    }
+    esp_lcd_panel_io_tx_param(io, command, NULL, 0);
+    return ESP_OK;
+}
---
+++ main/main.c
@@ -0,0 +1,113 @@
+
+#include <stdio.h>
+#include "driver/gpio.h"
+#include "driver/spi_master.h"
+#include "esp_lcd_panel_io.h"
+#include "esp_lcd_panel_vendor.h"
+#include "esp_lcd_panel_ops.h"
+#include "freertos/task.h"
+#include "driver/uart.h"
+
+//Pins: CLK, MOSI, RES, DC, BLK, CS, MSIO
+
+#define V_PIN_BLK           12
+#define V_PIN_SCLK          14
+#define V_PIN_MOSI          13
+#define V_PIN_DC            27
+#define V_PIN_CS_LCD        15
+#define V_PIN_RST           16
+#define V_PANEL_WIDTH       320
+#define V_PANEL_HEIGHT      240
+
+esp_err_t esp_lcd_new_panel_ili9341(const esp_lcd_panel_io_handle_t io, const esp_lcd_panel_dev_config_t *panel_dev_config, esp_lcd_panel_handle_t *ret_panel);
+
+void app_main(void)
+{
+    //IO Installation
+    //  BLK: GPIO
+    gpio_config_t bk_gpio_config = {
+        .mode = GPIO_MODE_OUTPUT,
+        .pin_bit_mask = 1ULL << V_PIN_BLK
+    };
+    ESP_ERROR_CHECK(gpio_config(&bk_gpio_config));
+    gpio_set_level(V_PIN_BLK, 1);
+    //  CLK, MSIO, MOSI: SPI
+    spi_bus_config_t buscfg = {
+        .sclk_io_num = V_PIN_SCLK,
+        .mosi_io_num = V_PIN_MOSI,
+        .miso_io_num = -1,
+        .quadwp_io_num = -1,
+        .quadhd_io_num = -1,
+        .max_transfer_sz = V_PANEL_WIDTH * 160 * sizeof(uint16_t),
+    };
+    ESP_ERROR_CHECK(spi_bus_initialize(SPI2_HOST, &buscfg, SPI_DMA_DISABLED));
+    esp_lcd_panel_io_handle_t io_handle = NULL;
+    esp_lcd_panel_io_spi_config_t io_config = {
+        .dc_gpio_num = V_PIN_DC,
+        .cs_gpio_num = V_PIN_CS_LCD,
+        .pclk_hz = 20 * 1000 * 1000,
+        .lcd_cmd_bits = 8,
+        .lcd_param_bits = 8,
+        .spi_mode = 0,
+        .trans_queue_depth = 10,
+        .on_color_trans_done = NULL,
+        .user_ctx = NULL,
+    };
+    ESP_ERROR_CHECK(esp_lcd_new_panel_io_spi((esp_lcd_spi_bus_handle_t)SPI2_HOST, &io_config, &io_handle));
+    esp_lcd_panel_handle_t panel_handle = NULL;
+    esp_lcd_panel_dev_config_t panel_config = {
+        .reset_gpio_num = V_PIN_RST,
+        .flags.reset_active_high = 0,
+        //.rgb_endian = LCD_RGB_ENDIAN_RGB,
+        .color_space = ESP_LCD_COLOR_SPACE_BGR,
+        .bits_per_pixel = 16,
+    };
+    ESP_ERROR_CHECK(esp_lcd_new_panel_ili9341(io_handle, &panel_config, &panel_handle));
+    ESP_ERROR_CHECK(esp_lcd_panel_reset(panel_handle));
+    ESP_ERROR_CHECK(esp_lcd_panel_init(panel_handle));
+    static unsigned char color_map[32]={
+        0xF8,0,0xF8,0,0xF8,0,0xF8,0,0xF8,0,0xF8,0,0xF8,0,0xF8,0,
+        0xF8,0,0xF8,0,0xF8,0,0xF8,0,0xF8,0,0xF8,0,0xF8,0,0xF8,0,
+    };
+
+    unsigned int iConX;
+    unsigned int iConY;
+    for(iConY = 0 ; iConY < 240 ; iConY++){
+        for(iConX = 0 ; iConX < 20 ; iConX++){
+            esp_lcd_panel_draw_bitmap(panel_handle, 0 + iConX * 16, 0 + iConY, 16 + iConX * 16, iConY + 1, color_map);
+        }
+    }
+    /*
+    esp_lcd_panel_draw_bitmap(panel_handle, iCon, iCon+1, 0, 0, color_map);
+
+
+    static unsigned char color_map[32]={0xFF,0xFF,0xFF,0xFF,0xFF};
+    unsigned short iCon;
+    iCon = 0 ;
+    while(1){
+        esp_lcd_panel_draw_bitmap(panel_handle, iCon, iCon+1, 0, 0, color_map);
+        vTaskDelay(100);
+        iCon++;
+        iCon %= 100;
+        printf(".");
+    }
+    */
+    /*
+    
+    
+    ESP_ERROR_CHECK(esp_lcd_panel_disp_off(panel_handle, false));
+    gpio_set_level(V_PIN_BLK, 0);
+    
+    unsigned int iCon;
+    static unsigned char color_map[32]={0xFF,0xFF,0xFF,0xFF,0xFF};
+    iCon = 0 ;
+    while(1){
+        esp_lcd_panel_draw_bitmap(panel_handle, iCon, iCon+1, 0, 0, color_map);
+        vTaskDelay(100);
+        iCon++;
+        printf(".");
+    }
+    
+    ESP_ERROR_CHECK(esp_lcd_panel_mirror(panel_handle, true, false));
+    */
+}
